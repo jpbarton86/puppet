36508aee329ca6f3b02918fbc838b1621e872dc3
John B <root@cookbook.(none)>
2016-03-24 15:03:41 +0000

    misc: ntp

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -1,5 +1,17 @@
-node 'cookbook', 'cookbook2' {
+node 'cookbook2' {
 	file { '/tmp/hello':
 	  content => "Hello, world\n",
 	}
+}
+
+node 'cookbook' {
+  include puppet
+
+#class { "ntp":
+#    servers    => [ '0.uk.pool.ntp.org iburst', '1.uk.pool.ntp.org iburst'],
+#    autoupdate => true,
+#    restrict   => [],
+#    enable     => true,
+#  }
+
 }
